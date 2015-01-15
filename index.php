3d34ead547153c21d5c75654d3cf88d38358c881
zulldesign <admin@zulldesign.ml>
2015-01-15 19:13:26 +0800

    l

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-  echo 'Ini beneran pake PHP lho (in bahasa) <br/>';
-  echo 'Current PHP version: ' . phpversion();
-  
-  echo '<br/>Created by @tonidermawan';
-  //phpinfo();
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require('./wp-blog-header.php');
 ?>
